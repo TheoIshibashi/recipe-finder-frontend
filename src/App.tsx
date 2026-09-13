9006d74ec8254e9ce27fa901d6bd3b4885679b54
Theo Ishibashi <theoshiba2005@gmail.com>
2026-09-13 19:13:47 -0300

    feat: fetch random recipe from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+import type { Recipe } from "./types/recipe";
+
 function App(){
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
+
+  useEffect(() =>{
+    fetch("http://localhost:8000/recipes/random")
+      .then((response) => response.json())
+      .then((data) =>{
+        setRecipe(data)
+      });
+
+  }, []);
+
   return(
     <main>
       <h1>Recipe Finder</h1>
+      {recipe && <p>Receita atual: {recipe.name}</p>}
     </main>
   )
 }
