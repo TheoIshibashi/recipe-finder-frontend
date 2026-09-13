3f9820070f6145725c1f2a34611c43174a33ed1d
Theo Ishibashi <theoshiba2005@gmail.com>
2026-09-13 19:52:44 -0300

    feat: handle loading and errors for random recipe

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import type { Recipe } from "./types/recipe";
 
 function App(){
   const [recipe, setRecipe] = useState<Recipe | null>(null);
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
 
   useEffect(() =>{
     fetch("http://localhost:8000/recipes/random")
@@ -13,9 +15,34 @@ function App(){
 
   }, []);
 
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+
+    fetch("http://localhost:8000/recipes/random")
+    .then((response) => {
+      if (!response.ok){
+        throw new Error("Resposta invalida")
+      }
+
+      return response.json();
+    })
+    .then((data) =>{
+      setRecipe(data)
+    })
+    .catch(() =>{
+      setError("ocorreu um erro inesperado.")
+    })
+    .finally(() =>{
+      setLoading(false);
+    })
+  }, []);
+
   return(
     <main>
       <h1>Recipe Finder</h1>
+      {loading && <p>Carregando...</p>}
+      {error && <p>Erro: {error}</p>}
       {recipe && <p>Receita atual: {recipe.name}</p>}
     </main>
   )
